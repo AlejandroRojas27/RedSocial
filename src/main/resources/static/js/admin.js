f3ce4102b84be96d158a0e4b1e70e96667bb490f
Alejandro Rojas <rmae2727@gmail.com>
2023-06-14 13:11:42 -0600

    Implementacion de autentificacion de email.

--- src/main/resources/static/js/admin.js
+++ src/main/resources/static/js/admin.js
@@ -1,28 +1,56 @@
 $(document).ready(function() {
 
-    loadUsers();
+    authEmail();
 
     $('users').DataTable();
 
 })
 
+function updateEmailOfUser(users){
+    for(let user of users){
+        if(user.email == localStorage.email){
+            let fullName = user.name + ' ' + user.lastName;
+            document.getElementById('userName').outerHTML = '<div class="nameStyle" id="userName">'+ fullName +'</div>';
+            return;
+        }
+    }
+}
+
+async function authEmail(){
+
+    if(localStorage.email == 'alex@gmail.com'){
+        loadUsers();
+    }else{
+         alert("User is not an admin")
+         window.location.href = 'login.html';
+         return;
+    }
+
+}
+
+function getHeaders() {
+    return {
+     'Accept': 'application/json',
+     'Content-Type': 'application/json',
+     'Authorization': localStorage.token
+   };
+}
+
 async function loadUsers(){
 
     const request = await fetch('api/users', {
         method: 'GET',
-        headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json',
-    },
-    })
+        headers: getHeaders()
+    });
 
     const users = await request.json()
 
+    updateEmailOfUser(users)
+
     let listOfUsersOnHTMLTable = ''
 
-    for (let user of users) {
-        let deleteButton = '<a href="#" onclick="deleteUser('+ user.id +')" > DEL </a>';
 
+    for (let user of users) {
         let del = '<button href="#" onclick="deleteUser('+ user.id +')"> DELETE </button>'
 
         let userHTML = '<tr><td>'+ user.id +'</td><td>'+user.name+ ' '+user.lastName+'</td><td>'+user.email+'</td><td>'+user.years+'</td><td> '+ del +' </td></tr>';
